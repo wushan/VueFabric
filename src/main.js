20ddb40cc917d4a1c1bb33f409b119c4ed81b648
wushan <chiangloveslynn@gmail.com>
2016-10-07 19:06:32 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import fabric from './assets/scripts/fabric'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
